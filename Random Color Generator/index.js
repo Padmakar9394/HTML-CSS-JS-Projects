08e8b25a0a47d1e8f8d9dee56781af16feb5e7ef
Padmakar9394 <prg2024btech@gmail.com>
2023-01-29 23:32:14 +0530

    created random-color-generator

---
+++ Random Color Generator/index.js
@@ -0,0 +1,22 @@
+const colorContainerEls = document.querySelectorAll(".item");
+
+function randomColor() {
+    const chars = "0123456789abcdef";
+    const colorCodeLength = 6;
+    let colorCode = "";
+    for (let index = 0; index < colorCodeLength; index++) {
+      const randomNum = Math.floor(Math.random() * chars.length);
+      colorCode += chars.substring(randomNum, randomNum + 1);
+    }
+    return colorCode;
+  }
+
+const setBg = () => {
+    colorContainerEls.forEach((colorContainerEl) => {
+        const newColorCode = randomColor();
+        colorContainerEl.style.backgroundColor = "#" + newColorCode;
+        colorContainerEl.innerText = "#" + newColorCode;
+      });
+}
+
+setBg();
